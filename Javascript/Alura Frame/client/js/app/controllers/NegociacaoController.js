d768afe08060cfaa83c086e89a04496b3f419f61
jhsmaciel <jhsmaciel15@gmail.com>
2019-04-17 15:25:18 -0300

    att

---
+++ Javascript/Alura Frame/client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,32 @@
+class NegociacaoController {
+    
+    constructor(){
+        let $ = document.querySelector.bind(document);
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+        this._listaNegociacao = new ListaNegociacao();
+        this._negociacoesView = new NegociacoesView($('#negociacoes-view'));
+        this._negociacoesView.update(this._listaNegociacao);
+    }
+
+    adiciona(event){
+        event.preventDefault();
+        this._listaNegociacao.adiciona(this._criaNegociacao());
+        this._negociacoesView.update(this._listaNegociacao);
+        this._limpaFormulario();
+    }
+    _criaNegociacao(){
+        return new Negociacao(  
+            DateHelper.textToDate(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+    }
+    _limpaFormulario(){
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0;
+        this._inputData.focus();
+    }
+}
