d9dc293c4604f5f311461b660a9854a17eb487d6
jhsmaciel <jhsmaciel15@gmail.com>
2019-04-25 14:25:11 -0300

    att

--- Javascript/NodeJS/Casadocode/src/app/views/livros/form/form.marko.js
+++ Javascript/NodeJS/Casadocode/src/app/views/livros/form/form.marko.js
@@ -9,8 +9,9 @@ var marko_template = module.exports = require("marko/src/html").t(__filename),
     marko_helpers = require("marko/src/runtime/html/helpers"),
     marko_loadTag = marko_helpers.t,
     component_globals_tag = marko_loadTag(require("marko/src/components/taglib/component-globals-tag")),
-    marko_escapeXmlAttr = marko_helpers.xa,
+    marko_forEach = marko_helpers.f,
     marko_escapeXml = marko_helpers.x,
+    marko_escapeXmlAttr = marko_helpers.xa,
     init_components_tag = marko_loadTag(require("marko/src/components/taglib/init-components-tag")),
     await_reorderer_tag = marko_loadTag(require("marko/src/taglibs/async/await-reorderer-tag"));
 
@@ -21,7 +22,27 @@ function render(input, out, __component, component, state) {
 
   component_globals_tag({}, out);
 
-  out.w("<header class=\"cabecalhoPrincipal\"><div class=\"container\"><div class=\"row align-items-center\"><div class=\"col-4\"><h1 class=\"logo\"><img src=\"/estatico/imagens/logo-casadocodigo.svg\" alt=\"Casa do Código\"></h1></div><div class=\"cabecalhoPrincipal-navegacao col-8\"><a href=\"#\" class=\"login\"><i class=\"fas fa-sign-in-alt\"></i>Login</a></div></div></div></header><main class=\"conteudoPrincipal\"><div class=\"container\"><h1>Cadastro de livros</h1><form action=\"/livros\" method=\"post\">");
+  out.w("<header class=\"cabecalhoPrincipal\"><div class=\"container\"><div class=\"row align-items-center\"><div class=\"col-4\"><h1 class=\"logo\"><img src=\"/estatico/imagens/logo-casadocodigo.svg\" alt=\"Casa do Código\"></h1></div><div class=\"cabecalhoPrincipal-navegacao col-8\"><a href=\"#\" class=\"login\"><i class=\"fas fa-sign-in-alt\"></i>Login</a></div></div></div></header><main class=\"conteudoPrincipal\"><div class=\"container\"><h1>Cadastro de livros</h1>");
+
+  if (data.errosValidacao) {
+    out.w("<div>");
+
+    var for__20 = 0;
+
+    marko_forEach(data.errosValidacao, function(erro) {
+      var keyscope__21 = "[" + ((for__20++) + "]");
+
+      out.w("<div class=\"alert alert-danger\">" +
+        marko_escapeXml(erro.param) +
+        " - " +
+        marko_escapeXml(erro.msg) +
+        "</div>");
+    });
+
+    out.w("</div>");
+  }
+
+  out.w("<form action=\"/livros/form\" method=\"post\">");
 
   if (data.livro.id) {
     out.w("<div><input type=\"hidden\" name=\"_method\" value=\"PUT\"><input type=\"hidden\" name=\"id\" value=\"" +
@@ -35,11 +56,11 @@ function render(input, out, __component, component, state) {
     marko_escapeXmlAttr(data.livro.preco) +
     "\" class=\"form-control\"></div><div class=\"form-group\"><label for=\"descricao\">Descrição:</label><textarea cols=\"20\" rows=\"10\" id=\"descricao\" name=\"descricao\" placeholder=\"fale sobre o livro\" class=\"form-control\">" +
     marko_escapeXml(data.livro.descricao) +
-    "</textarea></div><input type=\"submit\" value=\"Salvar\" class=\"btn btn-primary\"></form></div></main><footer class=\"rodape\"><div class=\"container\"><div class=\"row align-items-center\"><div class=\"col-4\"><img src=\"/estatico/imagens/logo-rodape.svg\" class=\"logo-rodape\"></div><div class=\"col-8\"><ul class=\"redesSociais\"><li><a href=\"\" class=\"compartilhar-facebook\" target=\"_blank\">/CasaDoCodigo</a></li><li><a href=\"\" class=\"compartilhar-twitter\" target=\"_blank\">@casadocodigo</a></li></ul></div></div></div></footer>");
+    "</textarea></div><input type=\"submit\" value=\"Salvar\" class=\"btn btn-primary\"></form></div></main><footer class=\"rodape\"><div class=\"container\"><div class=\"row align-items-center\"><div class=\"col-4\"><img src=\"/estatico/imagens/logo-rodape.svg\" class=\"logo-rodape\"></div><div class=\"col-8\"><ul class=\"redesSociais\"><li><a href=\"http://www.facebook.com/casadocodigo\" class=\"compartilhar-facebook\" target=\"_blank\">/CasaDoCodigo</a></li><li><a href=\"http://www.twitter.com/casadocodigo\" class=\"compartilhar-twitter\" target=\"_blank\">@casadocodigo</a></li></ul></div></div></div></footer>");
 
   init_components_tag({}, out);
 
-  await_reorderer_tag({}, out, __component, "44");
+  await_reorderer_tag({}, out, __component, "48");
 
   out.w("</body></html>");
 }
--- Javascript/NodeJS/Casadocode/src/config/custom-express.js
+++ Javascript/NodeJS/Casadocode/src/config/custom-express.js
@@ -6,6 +6,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 
+const templates = require('../app/views/templates');
+
 app.use('/estatico', express.static('src/app/public'));
 
 app.use(bodyParser.urlencoded({
@@ -23,15 +25,16 @@ app.use(methodOverride(function (req, res) {
 const rotas = require('../app/rotas/rotas');
 rotas(app);
 
-app.use(function(req,res,next){
-  return res.status(404).marko(
-    require('../app/views/base/404.marko'));
+app.use(function (req, resp, next) {
+    return resp.status(404).marko(
+        templates.base.erro404
+    );
 });
 
-app.use(function(erro,req,res,next){
-  return res.status(500).marko(
-    require('../app/views/base/500.marko'));
+app.use(function (erro, req, resp, next) {
+    return resp.status(500).marko(
+        templates.base.erro500
+    );
 });
-
 
 module.exports = app;
