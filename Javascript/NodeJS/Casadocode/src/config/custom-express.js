6902be36ac62e2c34d7a39ef6d4bb652a130c85e
jhsmaciel <jhsmaciel15@gmail.com>
2019-04-25 11:52:44 -0300

    att

--- Javascript/NodeJS/Casadocode/src/config/custom-express.js
+++ Javascript/NodeJS/Casadocode/src/config/custom-express.js
@@ -23,4 +23,15 @@ app.use(methodOverride(function (req, res) {
 const rotas = require('../app/rotas/rotas');
 rotas(app);
 
+app.use(function(req,res,next){
+  return res.status(404).marko(
+    require('../app/views/base/404.marko'));
+});
+
+app.use(function(erro,req,res,next){
+  return res.status(500).marko(
+    require('../app/views/base/500.marko'));
+});
+
+
 module.exports = app;
